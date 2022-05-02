c5e94c59f66401d33598ae9bc05b86147db63f0a
Juan Pablo Medina <Juampamedinadiaz2002@gmail.com>
2022-05-01 20:50:18 -0500

    -Cambio ruta carrreras y estudiante

--- src/routers/index.routers.js
+++ src/routers/index.routers.js
@@ -7,14 +7,14 @@ const EstudianteController = require('../controllers/estudiantes.controllers')
 
 const vs = "/api/v1"
 
-router.get(vs + '/estudiante', EstudianteController.consultarEstudiante)
-    .get(vs + '/estudiante', EstudianteController.consultarEstudiantes)
+router.get(vs + '/estudiante/:id', EstudianteController.consultarEstudiante)
+    .get(vs + '/estudiantes', EstudianteController.consultarEstudiantes)
     .post(vs + '/estudiante', EstudianteController.crearEstudiante)
     .delete(vs + '/estudiante/:id', EstudianteController.eliminarEstudiante)
     .put(vs + '/estudiante/:id', EstudianteController.actualizarEstudiante)
 
-router.get(vs + '/carrera/:id', CarreraController.consultarCarrera)
-    .get(vs + '/carrera', CarreraController.consultarCarreras)
+router.get(vs + '/carrera', CarreraController.consultarCarrera)
+    .get(vs + '/carreras', CarreraController.consultarCarreras)
     .post(vs + '/carrera', CarreraController.crearCarrera)
     .delete(vs + '/carrera/:id', CarreraController.eliminarCarrera)
     .put(vs + '/carrera/:id', CarreraController.actualizarCarrera)
